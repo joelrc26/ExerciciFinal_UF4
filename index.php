6ad956b0217f2849f268ad81f7bf95f9163f3a72
Joel Rodriguez <jrodriguez2@institutmvm.cat>
2023-05-11 19:44:38 +0200

    Añado persona4,php + persona5.php + index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 include "hola.php";
-echo "Listado de personas:\n";
+echo "Listado de personas:"
 include "persona.php";
-<<<<<<< HEAD
 include "persona2.php";
-=======
->>>>>>> joelRama2
 include "persona3.php";
+include "persona4.php";
+include "persona5.php";
 ?>
+
